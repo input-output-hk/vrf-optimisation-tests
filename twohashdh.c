58d64ed30772b981bce5f3e10c8a12887bc60b3b
iquerejeta <querejeta.azurmendi@iohk.io>
2021-06-28 15:46:55 +0100

    working unknown behavihour

--- twohashdh.c
+++ twohashdh.c
@@ -5,16 +5,18 @@ int main(void) {
 #define MESSAGE_LEN 22
     unsigned char message[MESSAGE_LEN] = "test_rust_verification";
 
-    unsigned char pk[crypto_vrf_twohashdh_PUBLICKEYBYTES];
     unsigned char sk[crypto_vrf_twohashdh_SECRETKEYBYTES];
 
-    crypto_vrf_twohashdh_keypair(pk, sk);
+    crypto_vrf_twohashdh_keypair(sk);
 
     unsigned char vrf_proof[crypto_vrf_twohashdh_PROOFBYTES];
-    crypto_vrf_twohashdh_prove(vrf_proof, sk, message, MESSAGE_LEN);
+    if (crypto_vrf_twohashdh_prove(vrf_proof, sk, message, MESSAGE_LEN) != 0) {
+        printf("failed generating proof\n");
+        return -1;
+    }
 
     unsigned char proof_output[crypto_vrf_twohashdh_OUTPUTBYTES];
-    if (crypto_vrf_twohashdh_verify(proof_output, pk, vrf_proof, message, MESSAGE_LEN) != 0) {
+    if (crypto_vrf_twohashdh_verify(proof_output, sk + 32, vrf_proof, message, MESSAGE_LEN) != 0) {
         printf("failed\n");
     }
     else {
