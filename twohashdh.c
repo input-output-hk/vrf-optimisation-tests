e8888135edfec2f8b74e8a6b8ffcdc5a740ffb98
iquerejeta <querejeta.azurmendi@iohk.io>
2021-06-29 16:19:37 +0100

    eval of twohashdh

--- twohashdh.c
+++ twohashdh.c
@@ -1,7 +1,10 @@
 #include <sodium.h>
+#include <time.h>
 
 int main(void) {
-
+    FILE *fpt;
+    fpt = fopen("results_2hashdh.csv", "w+");
+    fprintf(fpt,"verif\n");
 #define MESSAGE_LEN 22
     unsigned char message[MESSAGE_LEN] = "test_rust_verification";
 
@@ -16,11 +19,21 @@ int main(void) {
     }
 
     unsigned char proof_output[crypto_vrf_twohashdh_OUTPUTBYTES];
-    if (crypto_vrf_twohashdh_verify(proof_output, sk + 32, vrf_proof, message, MESSAGE_LEN) != 0) {
-        printf("failed\n");
+
+
+    for (int i = 0; i < 1000; i++){
+        clock_t t_api;
+        t_api = clock();
+        int result = crypto_vrf_twohashdh_verify(proof_output, sk + 32, vrf_proof, message, MESSAGE_LEN);
+        t_api = clock() - t_api;
+        double time_api = ((double) t_api) / CLOCKS_PER_SEC;
+
+        if (result != 0) {
+            printf("failed\n");
+        }
+
+        fprintf(fpt,"%f\n", time_api);
     }
-    else {
-        printf("passed\n");
-    }
+
     return 0;
 }	
