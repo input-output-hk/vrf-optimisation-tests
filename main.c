aefecb26f5789d91cf6025ad478d394c9bf24dcc
iquerejeta <querejeta.azurmendi@iohk.io>
2021-08-02 12:05:48 +0200

    c estimate ~x2

--- main.c
+++ main.c
@@ -6,8 +6,7 @@ int main(void) {
     FILE *fpt;
     fpt = fopen("Results.csv", "w+");
     fprintf(fpt,"verif, verif_opt, verif_opt_blake, verif_try_inc, batch_compatible, api_mul, internal_mul, "
-                "single_multi, 100_multi, 200_multi\n");
-
+                "single_multi, 20_multi, 100_multi\n");
 	#define MESSAGE_LEN 22
 	unsigned char message[MESSAGE_LEN] = "test_rust_verification";
 
@@ -15,16 +14,6 @@ int main(void) {
 	unsigned char sk[crypto_vrf_ietfdraft03_SECRETKEYBYTES];
 	
 	crypto_vrf_ietfdraft03_keypair(pk, sk);
-
-	unsigned char zero_point[32];
-    if (crypto_core_ed25519_sub(zero_point, pk, pk) == -1) {
-        printf("failed");
-    }
-    printf("zero bytes: ");
-    for (int i = 0; i < 32; i++) {
-        printf("%d", zero_point[i]);
-    }
-    printf("\n");
 
     unsigned char random_scalar[32];
     crypto_core_ed25519_scalar_random(random_scalar);
@@ -38,7 +27,7 @@ int main(void) {
     unsigned char vrf_proof_own[crypto_vrf_ietfdraft03_PROOFBYTES];
     crypto_vrf_ietfdraft03_prove_try_inc(vrf_proof_own, sk, message, MESSAGE_LEN);
     unsigned char proof_output[crypto_vrf_ietfdraft03_OUTPUTBYTES];
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 100; i++){
         unsigned char v[crypto_core_ed25519_BYTES];
         clock_t t_api;
         t_api = clock();
@@ -51,10 +40,9 @@ int main(void) {
         int int_mul = internal_scalarmul(pk, random_scalar);
         t_internal = clock() - t_internal;
         double time_internal = ((double) t_internal) / CLOCKS_PER_SEC;
-
         double time_single_multi_mult = time_per_proof(1);
-        double time_100_multi_mult = time_per_proof(20);
-        double time_200_multi_mult = time_per_proof_200();
+        double time_20_multi_mult = time_per_proof(20);
+        double time_100_multi_mult = time_per_proof(100);
 //        double time_1000_multi_mult = time_per_proof(1000);
 
         if (int_mul != 0) {
@@ -63,9 +51,7 @@ int main(void) {
 
         clock_t t_verif_batch_comp;
         t_verif_batch_comp = clock();
-
         int verification_batch = crypto_vrf_ietfdraft03_verify_batch_compatible(proof_output, pk, vrf_proof_batch_compatible, message, MESSAGE_LEN);
-
         t_verif_batch_comp = clock() - t_verif_batch_comp;
         double time_taken_verif_batch_comp = ((double) t_verif_batch_comp) / CLOCKS_PER_SEC;
 
@@ -132,7 +118,7 @@ int main(void) {
 
         fprintf(fpt,"%f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", time_taken_verif, time_taken_verif_opt,
                 time_taken_verif_blake, time_taken_verif_try_inc, time_taken_verif_batch_comp, time_api,
-                time_internal, time_single_multi_mult, time_100_multi_mult, time_200_multi_mult);
+                time_internal, time_single_multi_mult, time_20_multi_mult, time_100_multi_mult);
     }
 
     fclose(fpt);
